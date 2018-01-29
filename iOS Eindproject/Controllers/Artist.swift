78a3e456793a38a3d081874762795928c774c9f7
marnixsteel <marnix.steel@icloud.com>
2018-01-29 14:53:40 +0100

    file structure adjusted, class Artist made

---
+++ iOS Eindproject/Controllers/Artist.swift
@@ -0,0 +1,26 @@
+//
+//  Artist.swift
+//  iOS Eindproject
+//
+//  Created by Marnix Steel on 29/01/2018.
+//  Copyright © 2018 EhB. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    
+    var name: String
+    var stage: String
+    var time: String
+    
+    init (name: String, stage: String, time: String){
+        self.name = name;
+        self.stage = stage;
+        self.time = time;
+        
+    }
+    
+}
+
+
