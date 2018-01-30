e06de33e34e895076dbe5b687e6eb325e464eec8
ontlener <ontlener@macbook-air-van-ontlener.local>
2018-01-30 16:31:47 +0100

    de vissen zijn terug

--- iOS Eindproject/Controllers/ViewController.swift
+++ iOS Eindproject/Controllers/ViewController.swift
@@ -26,15 +26,15 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         bigMap.mapType = .hybrid
         bigMap.showsBuildings = true // displays buildings
         
-        let tstrand = CLLocationCoordinate2DMake(51.146294287889894, 2.707185745239258)
-        bigMap.region = MKCoordinateRegionMakeWithDistance(tstrand, 500, 50) // sets the visible region of the map
+        let tstrand = CLLocationCoordinate2DMake(51.152378316793744, 2.719169855117798)
+        bigMap.region = MKCoordinateRegionMakeWithDistance(tstrand, 1500, 200) // sets the visible region of the map
         
         // create a 3D Camera
         let mapCamera = MKMapCamera()
         mapCamera.centerCoordinate = tstrand
-        mapCamera.pitch = 45
-        mapCamera.altitude = 200 // example altitude
-        mapCamera.heading = 45
+        mapCamera.pitch = 60
+        mapCamera.altitude = 1600 // example altitude
+        mapCamera.heading = 55
         
         // set the camera property
         bigMap.camera = mapCamera
@@ -46,7 +46,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
         checkLocationIsOk()
+        
         /*
+        
         //aanmaken coördinaten
         let centerCoord:CLLocationCoordinate2D =
             CLLocationCoordinate2DMake(51.146294287889894, 2.707185745239258)
@@ -57,7 +59,11 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let centerRegion = MKCoordinateRegionMake (centerCoord, centerSpan)
         //regio aan kaart meegeven
         bigMap.region = centerRegion
-        */
+ 
+ */
+ 
+ 
+ 
         createPins()
         
     }
@@ -73,17 +79,17 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     func createPins(){
         //coordinaten aan maken, op welke plaats komen de pincoordinates
-        let ehbCoord = CLLocationCoordinate2DMake(51.135244, 2.732162)
+        let ehbCoord = CLLocationCoordinate2DMake(51.15200145335097, 2.7228713035583496)
         let ehbCoord2:CLLocationCoordinate2D =
-            CLLocationCoordinate2DMake(51.136550, 2.732110)
-        let ehbCoord3 = CLLocationCoordinate2DMake(51.136348, 2.733120)
-        let ehbCoord4 = CLLocationCoordinate2DMake(51.139251, 2.730530)
-        let ehbCoord5 = CLLocationCoordinate2DMake(51.130548, 2.731970)
+            CLLocationCoordinate2DMake(51.152378316793744, 2.727677822113037)
+        let ehbCoord3 = CLLocationCoordinate2DMake(51.149524845587585, 2.7161765098571777)
+        let ehbCoord4 = CLLocationCoordinate2DMake(51.15584397022548, 2.7167826890945435)
+        let ehbCoord5 = CLLocationCoordinate2DMake(51.15181302047539, 2.7191483974456787)
         //Pin aan maken via klasse
         let ehbPoint = MapPoint.init(coord: ehbCoord, title: "Vistival", subtitle: "Ontvangst")
         let ehbPoint2 = MapPoint.init(coord: ehbCoord2, title: "Vistival", subtitle: "Podium Garnaalvisser")
-        let ehbPoint3 = MapPoint.init(coord: ehbCoord3, title: "Vistival", subtitle: "Podium Zeezicht")
-        let ehbPoint4 = MapPoint.init(coord: ehbCoord4, title: "Vistival", subtitle: "Eventhal Proeverij")
+        let ehbPoint3 = MapPoint.init(coord: ehbCoord4, title: "Vistival", subtitle: "Podium Zeezicht")
+        let ehbPoint4 = MapPoint.init(coord: ehbCoord3, title: "Vistival", subtitle: "Eventhal Proeverij")
         let ehbPoint5 = MapPoint.init(coord: ehbCoord5, title: "Vistival", subtitle: "Kinderanimatie")
         // pin (annotation toevoegen aan kaart, mapview)
         bigMap.addAnnotation(ehbPoint)
